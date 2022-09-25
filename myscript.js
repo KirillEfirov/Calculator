7ad8b43c2ce13da0503e5aba6495e8db4ffc9087
Kirill <shishkokirill.fudosin@gmail.com>
2022-09-25 15:20:55 +0300

    You can get expression from user and use it in reversePolish function

--- myscript.js
+++ myscript.js
@@ -1,3 +1,74 @@
+const display = document.querySelector("#display");
+let br = document.querySelector("br");
+let expression = document.querySelector(".expression");
+let equals = document.querySelector(".equals");
+let button = document.querySelectorAll("button");
+
+
+const number = document.querySelectorAll(".number");
+
+//console.log(number[0].textContent);
+
+
+
+
+
+
+function getExpression() {
+    var str = " ";
+    for(const each of number) {
+        each.addEventListener("click", () => {
+            str += each.textContent;
+            return console.log(str);
+        });
+    }
+}
+getExpression();
+/*number[0].addEventListener("click", () => {
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add(a, b) {
+	return a + b;
+};
+function subtract(a, b) {
+	return a - b;
+};
+function sum(arr) {
+  let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+function multiply(arr) {
+  let multiply = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    multiply *= arr[i];
+  }
+
+  return multiply;
+};
+
+
+
+
+
+
+
+
 class Stack {
     constructor() {
         this.items = [];
@@ -29,5 +100,22 @@ class Stack {
         return str;
     }
 }
+let stack = new Stack();
 
-let Stack = new Stack();
+
+
+
+
+
+
+function reversePolish(expression) {
+    let expr = expression.split(" "); //turn String into array
+    const array = []; 
+    if(expr === '') {
+        return 0;
+    }
+
+    console.log(reversePolish('1 3 5 * -'));
+
+}
+
